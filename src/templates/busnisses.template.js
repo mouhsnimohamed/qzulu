6dafdb1d26647b2f332b8ae58593a4378a2bf9e7
mouhsnimohamed <m.mouhsni@gmail.com>
2020-03-28 12:51:12 +0100

    add new teplate

--- src/templates/busnisses.template.js
+++ src/templates/busnisses.template.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 import BusinessFeatures from '../components/BusnissesFeatures';
 
-export const BusnissesTemplate = ({ title, intro, blocks }) => {
+export const BusnissesTemplate = ({ title, intro, blocks = [] }) => {
   return (
     <Layout>
       <div className="content">
@@ -18,17 +18,9 @@ export const BusnissesTemplate = ({ title, intro, blocks }) => {
         <div className="columns is-multiline">
           <div className="column is-10 is-offset-1">
             <p className="costum-p">{intro}</p>
+            {blocks && !!blocks.length && <BusinessFeatures blocks={blocks} />}
           </div>
         </div>
-        <section className="section section--gradient">
-          <div className="container content">
-            <div className="columns is-multiline">
-              <div className="column is-10 is-offset-1">
-                <BusinessFeatures blocks={blocks} />
-              </div>
-            </div>
-          </div>
-        </section>
       </div>
     </Layout>
   );
